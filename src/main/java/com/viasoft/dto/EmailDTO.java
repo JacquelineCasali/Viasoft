ad1791973d55d59b053f93f7fa56ae92c7dedda4
Jacqueline <jcasalia@yahoo.com.br>
2025-05-16 15:29:21 -0300

    criando o service

--- src/main/java/com/viasoft/dto/EmailDTO.java
+++ src/main/java/com/viasoft/dto/EmailDTO.java
@@ -5,18 +5,18 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public class EmailDTO {
-    @NotBlank(message = "Preencha Email do destinatário")
+    @NotBlank(message = "Preencha Email do destinatário campo obrigatório ")
     @Email(message = "Email inválido")
     private String emailDestinatario;
-    @NotBlank(message = "Preencha o Nome")
+    @NotBlank(message = "Preencha o Nome do destinatário campo obrigatório")
     private String nomeDestinatario;
-    @NotBlank(message = "Preencha Email do Remetente")
+    @NotBlank(message = "Preencha Email do Remetente campo obrigatório")
     @Email(message = "Email inválido")
     private String emailRemetente;
-    @NotBlank(message = "Preencha o Assunto do Email")
+    @NotBlank(message = "Preencha o Assunto do Email campo obrigatório")
     private String assunto;
 
-    @NotBlank(message = "Preencha o conteudo do Email")
+    @NotBlank(message = "Preencha o conteudo do Email campo obrigatório")
     private String conteudo;
 
     public String getEmailDestinatario() {
